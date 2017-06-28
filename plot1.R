264eb351eb2606571392d1ebeef52bd148df4b17
Udofia /datasciencecoursera <santadogo76@users.noreply.github.com>
2017-06-28 01:01:39 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,17 @@
+Question 1
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
+# for each of the years 1999, 2002, 2005, and 2008.
 
+## Firstly, aggregate the data in
+totalEmission <- aggregate(Emissions ~ year, NEIdata, sum)
+totalEmission
+
+## Plotting the total Emissions over time using a base plotting in
+barplot(
+  (totalEmission$Emissions)/10^6,
+  names.arg=totalEmission$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (10^6 Tons)",
+  main="Total PM2.5 Emissions From All US Sources"
+)
