24d6c3edb784213f86c6068bc211a7541cbb6832
Udofia /datasciencecoursera <santadogo76@users.noreply.github.com>
2017-06-28 00:40:23 -0500

    Update downloadArchieve.R

--- downloadArchieve.R
+++ downloadArchieve.R
@@ -1,4 +1,4 @@
-## Data Preparation
+ Data Preparation
 
 # Download archive file, if it does not exist
 archiveFile <- "NEI_data.zip"
@@ -16,3 +16,15 @@ if(!(file.exists("summarySCC_PM25.rds") &&
 ## We now load the NEI and SCC data frames from the .rds file
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds
+
+Further Pre-processing of the data is done
+## Converting "year", "type", "Pollutant", "SCC", "fips" to factor
+colToFactor <- c("year", "type", "Pollutant","SCC","fips")
+NEI[,colToFactor] <- lapply(NEI[,colToFactor], factor)
+
+head(levels(NEI$fips))
+
+## The levels have NA as "   NA", so converting that level back to NA
+levels(NEI$fips)[1] = NA
+NEIdata<-NEI[complete.cases(NEI),]
+colSums(is.na(NEIdata))
