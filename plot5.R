8f51a9d217ac121f8541fe6f1ac8652ff22e4e26
Udofia /datasciencecoursera <santadogo76@users.noreply.github.com>
2017-06-28 04:26:33 -0500

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -1,1 +1,21 @@
+Question 5:
+# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
 
+## First we subset the motor vehicles, which we assume is anything like Vehicle in EISector column
+SCCvehicle<-grepl(pattern = "vehicle", SCC$EISector, ignore.case = TRUE)
+SCCvehicleSCC <- SCC[SCCvehicle,]$SCC
+
+## using this boolean vector get the interested rows from the baltimore data
+NEIvehicleSSC <- NEIdata[NEIdata$SCC %in% SCCvehicleSCC, ]
+NEIvehicleBaltimore <- subset(NEIvehicleSSC, fips == "24510")
+NIEvehicleBaltimoreTotEm<-aggregate(Emissions~year, NEIvehicleBaltimore, su
+
+## Plotting the year-Emissions  
+g<-ggplot(aes(year, Emissions/10^5), data=NIEvehicleBaltimoreTotEm)
+g+geom_bar(stat="identity",fill="grey",width=0.75) +
+  guides(fill=FALSE) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
+                                    
+
+                                    
