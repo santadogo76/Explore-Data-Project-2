9e8c1d02102d96d0c4ccbbdf6af2427d212d0136
Udofia /datasciencecoursera <santadogo76@users.noreply.github.com>
2017-06-28 04:36:30 -0500

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,1 +1,23 @@
+Question 6:
+# Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources 
+# in Los Angeles County, California (fips == 06037). Which city has seen greater changes over time 
+# in motor vehicle emissions?
 
+## NOTE:Subset the VehiclesSSC based on fips and add a column for the city name, 
+# then combine the two data frame to generate data for both cities
+NEIvehicleBalti<-subset(NEIvehicleSSC, fips == "24510")
+NEIvehicleBalti$city <- "Baltimore City"
+NEIvehiclela<-subset(NEIvehicleSSC, fips == "06037")
+NEIvehiclela$city <- "Los Angeles County"
+NEIBothCity <- rbind(NEIvehicleBalti, NEIvehiclela)
+
+## Plot the result by facets for each city
+ggplot(NEIBothCity, aes(x=year, y=Emissions, fill=city)) +
+  geom_bar(aes(fill=year),stat="identity") +
+ facet_grid(.~city) +
+ guides(fill=FALSE) + theme_bw() +
+ labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+ labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
+
+ANSWER:
+# From the plot,  Los Angeles County has seen greater changes over time in vehicle emissions
